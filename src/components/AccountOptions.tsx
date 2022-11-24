ba56f1aaff9d54f5d14ae127769cade117f919cc
Nikhil Ranjan <niklabh811@gmail.com>
2022-11-24 17:35:24 +0100

    Style Fixes (#5)

--- src/components/AccountOptions.tsx
+++ src/components/AccountOptions.tsx
@@ -1,4 +1,4 @@
-import { Button, Menu, MenuItem } from "@mui/material";
+import { Button, FormControl, InputLabel, Select, MenuItem } from "@mui/material";
 import React from "react";
 import { Account } from "../types/chain";
 
@@ -13,56 +13,27 @@ const AccountOptions = ({
   accounts,
   label,
 }: AccountOptionsProps) => {
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
-  //const [activeAccount, setAccount] = useState<Account>();
-  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-  const handleSelectAccount = (account: Account) => {
-    onSelectAccount(account);
-    // close the floating options after the user selects something
-    handleClose();
+  const handleSelectAccount = (event: any) => {
+    const index: number = event.target.value;
+    onSelectAccount(accounts[index]);
   };
   return (
-    <>
-      <Button
-        id="demo-positioned-button"
-        aria-controls={open ? "demo-positioned-menu" : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? "true" : undefined}
-        onClick={handleClick}
-        disabled={accounts.length < 1}
-      >
-        {label}
-      </Button>
-      <Menu
+    <FormControl fullWidth style={{margin: '10px'}}>
+      <InputLabel id="demo-simple-select-label">From</InputLabel>
+      <Select
         id="demo-positioned-menu"
         aria-labelledby="demo-positioned-button"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        anchorOrigin={{
-          vertical: "top",
-          horizontal: "left",
-        }}
-        transformOrigin={{
-          vertical: "top",
-          horizontal: "left",
-        }}
+        onChange={handleSelectAccount}
       >
-        {accounts.map((i) => {
+        {accounts.map((account, index) => {
           return (
-            <MenuItem key={i.address} onClick={() => handleSelectAccount(i)}>
-              ({i.name}) {i.address}
+            <MenuItem key={account.address} value={index}>
+              ({account.name}) {account.address} {index}
             </MenuItem>
           );
         })}
-      </Menu>
-    </>
+      </Select>
+    </FormControl>
   );
 };
 
