32f4ef35f57f52cb7089804d7eaa2490f4104322
Hoon Kim <mail@hoonkim.me>
2022-11-25 17:02:33 +0100

    remove .env

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // deploy the contract and fill in the contract address
 export const config = {
   substrateEndpoint: 'ws://127.0.0.1:9944',
-  psp22NativeAddr: process.env.REACT_APP_PSP22_NATIVE_ADDRESS || '',
-  erc20NativeAddr: process.env.REACT_APP_ERC20_NATIVE_ADDRESS || '',
-  erc20EvmAddr: process.env.REACT_APP_ERC20_EVM_ADDRESS || '',
+  psp22NativeAddr: '',
+  erc20NativeAddr: '',
+  erc20EvmAddr: '',
 };
