0bd95c42167725208ef1ed9d284314a3f7f69e61
Hoon Kim <mail@hoonkim.me>
2022-12-21 15:45:25 +0100

    update erc20 contract

--- contracts/erc20.sol
+++ contracts/erc20.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.7.3/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.8/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
 
